c453cad48f5642b2d32be6ce73261b1feb9d521f
sebastiaciudad <sebastia.ciudad@gmail.com>
2021-05-28 10:56:05 +0200

    Update
    S'afageix una representacio dels dits i els ponts es creen

---
+++ PoseNetOSC-Unity-master/Assets/ThePath/Scripts/CreateBridge.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateBridge : MonoBehaviour
+{
+    public GameObject Dedo2;
+    public GameObject Cubo;
+
+    public bool active_bridge = false;
+
+    Vector3 PosicionDedo1;
+    Vector3 PosicionDedo2;
+    Vector3 Centro;
+
+    float Angulo_Radianes;
+    float Angulo_Grados;
+    float cont;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cont = 0;
+        Instanciar_puente();
+        Cubo.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(active_bridge == true)
+        {
+            if(cont == 0)
+            {
+                Cubo.SetActive(true);
+                cont = 1;
+            }
+            Calc_PR();
+        }
+
+        if(active_bridge == false && cont == 1)
+        {
+            cont = 0;
+            Cubo.SetActive(false);
+        }
+    }
+
+    void Instanciar_puente()
+    {
+        Calc_PR();
+        Cubo = Instantiate(Cubo, new Vector3(Centro.x, Centro.y, 0), Quaternion.identity, transform);
+        Cubo.transform.parent = null;
+    }
+
+    void Calc_PR()
+    {
+        PosicionDedo1 = transform.position;
+        PosicionDedo2 = Dedo2.transform.position;
+
+        Centro = (PosicionDedo1 + PosicionDedo2) / 2;
+
+        float hipotenusa = Mathf.Sqrt(Mathf.Pow(PosicionDedo2.x - PosicionDedo1.x, 2) + Mathf.Pow(PosicionDedo2.y - PosicionDedo1.y, 2));
+        Angulo_Radianes = Mathf.Asin((PosicionDedo2.y - PosicionDedo1.y) / hipotenusa);
+        Angulo_Grados = Angulo_Radianes * Mathf.Rad2Deg;
+
+        Cubo.transform.rotation = Quaternion.Euler(0, 0, Angulo_Grados);
+        Cubo.transform.position = new Vector3(Centro.x, Centro.y, 0);
+        Cubo.transform.localScale = new Vector3(hipotenusa, 1, 1);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (cont == 0)
+        {
+            active_bridge = true;
+        }
+        if (cont == 1)
+        {
+            active_bridge = false;
+        }
+    }
+}
