2023122ed0d105cdf8e48f78878007fdd393fece
perecb <56824254+perecb@users.noreply.github.com>
2021-06-14 21:04:06 +0200

    Music all the scenes

---
+++ ThePath/Assets/ThePath/Scripts/PlayMusic.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMusic : MonoBehaviour
+{
+    private PlayMusic instance;
+
+    public PlayMusic Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if(FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+
+        if(instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+}
